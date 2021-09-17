952594ce98ce0f0c8ecbb5d3cf1980b37ec0e198
liyongchuang <liyongchuang@yxt.com>
2021-09-17 18:42:57 +0800

    初始化项目

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
+/*
+ * @FileDescription:
+ * @Author: 李永创
+ * @Date: 2021-09-17 17:44:57
+ * @LastEditors: 李永创
+ * @LastEditTime: 2021-09-17 17:53:04
+ */
 import Vue from "vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
 Vue.config.productionTip = false;
+
+Vue.use(ElementUI);
 
 new Vue({
   router,
